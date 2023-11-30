a5900f873d84629ab93d043ca30397a9933adfbb
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2023-11-30 06:36:21 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,187 @@
+// www.codewars.com
+// December 1, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 2, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 3, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 4, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 5, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 6, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 7, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 8, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 9, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 10, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 11, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 12, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 13, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 14, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 15, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 16, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 17, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 18, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 19, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 20, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 21, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 22, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 23, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 24, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 25, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 26, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 27, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 28, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 29, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 30, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
+// December 31, 2023 Daily Katas
+// KATA 1 7kyu
+
+// KATA 2 7kyu
+
+// KATA 3 7kyu
